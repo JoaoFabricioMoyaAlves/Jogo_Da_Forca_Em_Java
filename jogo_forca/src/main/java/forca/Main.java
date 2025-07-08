e84d9face07daf91fdb9df661c66e3d0887873a0
joao_fabricio <joaofabriciomoyaalvesoficial@gmail.com>
2025-07-08 11:26:42 -0300

    montando a forca e os espaços de letras - referente a aula 2 do desafio

--- jogo_forca/src/main/java/forca/Main.java
+++ jogo_forca/src/main/java/forca/Main.java
@@ -3,14 +3,16 @@ package forca;
 import java.util.stream.Stream;
 
 import forca.model.HangmanChar;
+import forca.model.HangmanGame;
 
 public class Main {
     public static void main(String... args) {
-       var letter = Stream.of(args)
+       var characters = Stream.of(args)
                     .map(a -> a.toLowerCase().charAt(0))
                     .map(HangmanChar::new).toList();
         
-        System.out.println(letter);
+        System.out.println(characters);
+        System.out.println(new HangmanGame(characters));
 
     }
 }
